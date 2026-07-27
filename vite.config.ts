c57b68de3d9d15c8e342533df64f93c705b41f75
SILVAThiagoFerreira <13452222+SILVAThiagoFerreira@users.noreply.github.com>
2026-07-27 15:40:08 +0100

    fix: correct pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_REPOSITORY ? "/analizador-pre-corte-opitdev/" : "/",
+  base: process.env.GITHUB_REPOSITORY ? "/analizador-de-pre-corte-opitdev/" : "/",
   build: {
     target: "es2020",
     sourcemap: true
